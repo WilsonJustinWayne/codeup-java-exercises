de462bf57c9e3c1a52e6fd27f88213f99cd202ee
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-07-24 14:54:11 -0500

    first control flow exercise done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,8 +2,22 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.printf("%s ", i);
+//            i++;
+//        }
+//        System.out.println();
 
+//        long i = 2L;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000);
 
+//        for (long i = 2L; i < 1000000; i *= i) {
+//            System.out.println(j);
+//        }
     }
 
 }
