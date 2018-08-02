76be83283bf296a0919c5b66d429ffe00f2c2460
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-08-02 11:28:49 -0500

    finishing abstract exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,11 +3,10 @@ package shapes;
 public class ShapesTest {
 
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(4, 5);
-        System.out.println(box1.getPerimeter());
-        System.out.println(box1.getArea());
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getPerimeter());
-        System.out.println(box2.getArea());
+        Measurable myShape;
+        myShape = new Square(5);
+        System.out.println(myShape.getArea() + ", " + myShape.getPerimeter());
+        myShape = new Rectangle(4, 5);
+        System.out.println(myShape.getArea() + ", " + myShape.getPerimeter());
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,20 +1,27 @@
 package shapes;
 
-public class Square extends Rectangle {
-    private double side;
-
-    public Square(double side) {
-        super(side,side);
-        this.side = side;
+public class Square extends Quadrilateral {
+    Square(double side) {
+        super(side, side);
     }
 
     @Override
-    public double getArea() {
-        return Math.pow(side, 2);
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        this.width = width;
     }
 
     @Override
     public double getPerimeter() {
-        return 4 * side;
+        return 2 * length + 2 * width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
     }
 }
